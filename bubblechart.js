a6b0a1db1a0502a9c2de2981f8b6fa324ed4a7a1
Renee <11174747+rvanhoek@users.noreply.github.com>
2018-03-18 14:17:38 +0100

    add changes to bubblechart

--- bubblechart.js
+++ bubblechart.js
@@ -1,29 +1,22 @@
-var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    width = $("#stackedArea").width() - margin.left - margin.right,
-    height = $("#stackedArea").height() - margin.top - margin.bottom;
-
-/*
- * value accessor - returns the value to encode for a given data object.
- * scale - maps value to a visual display encoding, such as a pixel position.
- * map function - maps from data value to display value
- * axis - sets up axis
- */
+var margin = {top: 20, right: 20, bottom: 20, left: 10},
+    width = $("#bubbleChart").width(),
+    height = $("#bubbleChart").height();
 
 // setup x
-var xValue = function(d) { return d.religpct;}, // data -> value
-    xScale = d3.scaleLinear().range([0, width]), // value -> display
-    xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.axisBottom(xScale);
+var xValue = function(d) { return d.year + Math.random() *2 -1 ;}, // data -> value
+    xScaleB = d3.scaleLinear().range([margin.left, width-margin.left*2]), // value -> display
+    xMap = function(d) { return xScaleB(xValue(d));}, // data -> display
+    xAxis = d3.axisBottom()
+              .scale(xScaleB)
+              .tickFormat(d3.format("d"));
 
 // setup y
-var yValue = function(d) { return d["uni_per_pop"];}, // data -> value
-    yScale = d3.scaleLinear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis =  d3.axisLeft(yScale);
-
-// setup fill color
-var cValue = function(d) { return d.country;};
-var colorBubble =d3.scaleOrdinal(d3.schemeCategory10);
+var yValue = function(d) { return d.religpct;}, // data -> value
+    yScaleB = d3.scaleLinear().range([height, margin.bottom]), // value -> display
+    yMap = function(d) { return yScaleB(yValue(d));}, // data -> display
+    yAxis =  d3.axisRight()
+                .scale(yScaleB)
+                .ticks(10);
 
 
 // add the graph canvas to the body of the webpage
@@ -33,14 +26,14 @@ var bubbles = d3.select("#bubbleChart").append("svg")
 
 
 // add the tooltip area to the webpage
-var tooltip = d3.select("bubbleChart").append("div")
+var tooltip = d3.select("#bubbleChart").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
 
 
 // load data
-d3.csv("./data/rel_uni_pop.csv", function(error, data) {
+d3.csv("./data/rel_uni_pop_hpi.csv", function(error, data) {
 
   // change string (from CSV) into number format
   data.forEach(function(d) {
@@ -50,18 +43,19 @@ d3.csv("./data/rel_uni_pop.csv", function(error, data) {
     d.nonreligpct = +d.nonreligpct * 100;
     d.religpct = +d.religpct * 100;
     d.uni_per_pop = +d.uni_per_pop;
+    d.continent = d.continent;
+    d.popsize = +d.popsize;
     //console.log(d);
   });
 
-  // don't want dots overlapping axis, so add in buffer to data domain
-  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-  yScale.domain([d3.min(data, yValue)-0.03, d3.max(data, yValue)+0.3]);
+  xScaleB.domain([d3.min(data, xValue)-2, d3.max(data, xValue)+3]);
+  yScaleB.domain([0, d3.max(data, yValue)+3]);
 
-  // setup size of bubbles
+  // size of bubbles
   var scaleRadius = d3.scaleLinear()
-            .domain([d3.min(data, function(d) { return d.year; }),
-                    d3.max(data, function(d) { return d.year; })])
-            .range([2,10]);
+            .domain([d3.min(data, function(d) { return d.popsize; }),
+                    d3.max(data, function(d) { return d.popsize; })])
+            .range([4,10]);
 
 
   // x-axis
@@ -69,47 +63,80 @@ d3.csv("./data/rel_uni_pop.csv", function(error, data) {
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", 30)
-      .style("text-anchor", "end")
-      .text("Percentage religious");
+
+      // text label
+  bubbles.append("text")
+      .attr("transform",
+            "translate(" + (width/2) + " ," +
+                           (height + margin.top + 20) + ")")
+      .style("text-anchor", "middle")
+      .text("Year");
 
   // y-axis
   bubbles.append("g")
       .attr("class", "y axis")
+      .attr("transform", "translate(" + width + ",0)")
       .call(yAxis)
-    .append("text")
-      .attr("class", "label")
+
+      // text label
+  bubbles.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Number of universities per 100.000 people");
+      .attr("y", width+13)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("font", "12px")
+      .text("Percentage of pupulation that is religious");
 
   // draw dots
-  bubbles.selectAll(".dot")
+
+
+  bubbles.selectAll("path")
       .data(data)
     .enter().append("circle")
-      .filter(function(d) { return d.year == 2010 })
-      .attr("class", "dot")
+    //  .filter(function(d) { return d.country == activeCountry })
+      .attr("class", "bubbles")
+      .attr("d", bubbles)
       .attr("cx", xMap)
       .attr("cy", yMap)
-      .attr('r', function(d) { return scaleRadius(d.year)})
-      .style("fill", function(d,i) { return colorBubble(i);})
+      .attr('r', function(d) { return scaleRadius(d.popsize)})
+      .style("fill", function(d) {
+        if (d.continent == "Europe") {
+          return "red";
+        } else if (d.continent == "Asia") {
+          return "blue";
+        } else if (d.continent == "North America") {
+          return "green";
+        } else if (d.continent == "South America") {
+          return "orange";
+        } else if (d.continent == "Oceania") {
+          return "purple";
+        } else if (d.continent == "Africa") {
+          return "lightblue";
+        }
+        return "black";
+        })
       .on("mouseover", function(d) {
-          tooltip.transition()
-               .duration(200)
-               .style("opacity", .9);
-          tooltip.html(d["country"] + " in " + d["year"] + "<br/> (" + xValue(d)
-	        + "% religious, " + yValue(d) + " universities)")
+          activeCountry = d.country
+          console.log(activeCountry);
+
+          d3.select(this).classed("bubblesLight", true);
+          tooltip.html(d["country"] + " in " + d["year"] + "<br/> ("
+            + yValue(d) + "% religious)")
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
+          d3.selectAll(".country")
+         				.classed("country-on", function(d){
+         					if(d.properties.name == activeCountry) return true;
+         					else return false;
+         				})
       })
-      .on("mouseout", function(d) {
-          tooltip.transition()
-               .duration(500)
-               .style("opacity", 0);
+      .on('mouseout', function(d){
+        d3.select(this).classed("bubblesLight", false);
+        tooltip.classed("hidden", true);
+
+       // turn back the map
+ 			  d3.selectAll(".country-on")
+ 				.attr("class", "country");
       });
 });

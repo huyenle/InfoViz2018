1e7d496979e0f53ec356096c8103b24997f4ded8
Renee <11174747+rvanhoek@users.noreply.github.com>
2018-03-21 15:21:47 +0100

    filter by checkboxes

--- bubblechart.js
+++ bubblechart.js
@@ -95,12 +95,27 @@ d3.csv("./data/rel_uni_pop_hpi.csv", function(error, data) {
 
   // draw dots
 
+  // filter for continent
+  d3.selectAll(".contCheckbox").on("change",update);
+  update();
+
+  function update(){
+    var choices = [];
+    d3.selectAll(".contCheckbox").each(function(d){
+      cb = d3.select(this);
+      if(cb.property("checked")){
+        choices.push(cb.property("value"))
+        console.log(choices);
+      }
+    });
+
+  bubbles.selectAll("#bubblecircle").remove()
 
   bubbles.selectAll("bubbles")
       .data(data)
-    .enter().append("circle")
+      .enter().append("circle")
       .attr("id", "bubblecircle")
-    //  .filter(function(d) { return d.country == activeCountry })
+      .filter(function(d) { return choices.includes(d.continent) })
       .attr("class", "bubbles")
       .attr("d", bubbles)
       .attr("cx", xMap)
@@ -151,4 +166,6 @@ d3.csv("./data/rel_uni_pop_hpi.csv", function(error, data) {
         d3.selectAll(".country-on")
           .attr("class", "country");
       });
+
+  }
 });
